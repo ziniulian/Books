a763c047ff9c092d70dd8913021af7106393603b
ziniulian <ziniulian@163.com>
2017-05-19 16:57:27 +0800

    取消文件映射，只映射文件夹

--- 映射指令.bat
+++ 映射指令.bat
@@ -1,7 +1,4 @@
 @echo off
 mklink /D "L:\Doc\SVNsrv\Books\db\revprops" "L:\Doc\Git\Books\K"
 mklink /D "L:\Doc\SVNsrv\Books\db\revs" "L:\Doc\Git\Books\V"
-mklink "L:\Doc\SVNsrv\Books\db\rep-cache.db" "L:\Doc\Git\Books\D"
-mklink "L:\Doc\SVNsrv\Books\db\current" "L:\Doc\Git\Books\C"
-mklink "L:\Doc\SVNsrv\Books\db\txn-current" "L:\Doc\Git\Books\T"
 pause
